88a00d53479c75bead8d67035f1ffecdee6d93e4
hollandgeng <jingwei1998@hotmail.com>
2023-08-28 11:20:23 +0800

    Initial Commit

---
+++ Bookstore/BookstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookstoreApp.swift
+//  Bookstore
+//
+//  Created by Jing Wei on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
